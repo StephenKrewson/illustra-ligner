653bfa8d43941a4b9a0617343050263ff4d88de8
Rachel Prince <rachaprince@gmail.com>
2016-11-17 07:31:36 -0500

    saving the original method for comparing hist before a revamp

--- instanceCluster.py
+++ instanceCluster.py
@@ -1,35 +1,93 @@
-# Adapted from
+# Inspiration from
 # http://www.pyimagesearch.com/2014/07/14/3-ways-compare-histograms-using-opencv-python/
 # http://study.marearts.com/2014/11/opencv-emdearth-mover-distance-example.html
+
+# Ideas 1. have a "set book", and compare images to this set (nearest-nieghbor)
+# 1b. Find a better way to represent the results
+# 2. Turn the histogram into a channel
 import io
+import os
+import glob
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
-from scipy.spatial import distance as dist
 from emd import emd
+import argparse
 
-imgA = cv2.imread("./image1.jpg")
-imgB = cv2.imread("./image2.jpg")
+THRESHOLD = .90 # TBD later
 
-# Converting from BGR to HSV for color comparison
-cv2.cvtColor(imgA, cv2.COLOR_BGR2HSV)
-cv2.cvtColor(imgB, cv2.COLOR_BGR2HSV)
+def make2DHist(img):
+    i = cv2.imread(img)
+    cv2.cvtColor(i, cv2.COLOR_BGR2HSV)
 
-# Creating a 2D Histogram
-channels = [0,1]
-histSize = [180, 256]
-ranges = [0,180, 0, 256]
+    channels = [0,1]
+    histSize = [180, 256]
+    ranges = [0,180, 0, 256]
 
-histA = cv2.calcHist([imgA],channels,None,histSize, ranges)
-histB = cv2.calcHist([imgB],channels,None,histSize, ranges)
+    hist = cv2.calcHist([i],channels,None,histSize, ranges)
 
-# normalizing
-histA = cv2.normalize(histA,0, 1, cv2.NORM_MINMAX).flatten()
-histB = cv2.normalize(histB,0, 1, cv2.NORM_MINMAX).flatten()
+    # normalizing
+    hist = cv2.normalize(hist,0, 1, cv2.NORM_MINMAX).flatten()
 
-# compare similarity using earth mover distance
-# pip install -e git+https://github.com/garydoranjr/pyemd.git#egg=pyemd
-e = emd([histA], [histB])
-e = e * 100
-print 'similarity:'
-print e
+    return hist
+
+
+# imgA and imgB are paths to files
+def compareTwoHist(img1, img2, method, opt_args=None):
+
+     imgA = cv2.imread(img1)
+     imgB = cv2.imread(img2)
+
+     # Converting from BGR to HSV for color comparison
+     cv2.cvtColor(imgA, cv2.COLOR_BGR2HSV)
+     cv2.cvtColor(imgB, cv2.COLOR_BGR2HSV)
+
+     # Creating a 2D Histogram
+     # hue and saturation
+     channels = [0,1]
+     histSize = [180, 256]
+     ranges = [0,180, 0, 256]
+
+     histA = cv2.calcHist([imgA],channels,None,histSize, ranges)
+     histB = cv2.calcHist([imgB],channels,None,histSize, ranges)
+
+     # normalizing
+     histA = cv2.normalize(histA,0, 1, cv2.NORM_MINMAX).flatten()
+     histB = cv2.normalize(histB,0, 1, cv2.NORM_MINMAX).flatten()
+
+     # compare similarity using earth mover distance or opencv compareHist methods
+     # pip install -e git+https://github.com/garydoranjr/pyemd.git#egg=pyemd
+     delta = method([histA], [histB] ,opt_args)
+     return img1 + ' ' + img2 + ' similarity: ' + str(delta)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--images", required = True,
+	   help = "Path to the directory of images")
+    args = parser.parse_args()
+
+    image_paths = []
+
+    for f in glob.glob(args.images + "/*.jpg"):
+        image_paths.append(f)
+
+    f = open("pairwise_similarity.txt", 'w')
+
+    # find a prefix to determine which book to use
+    prefix = image_paths[0].split('/', 2)
+    prefix = prefix[2].split('_',1)[0]
+
+    # make k classes from one book
+    histograms = []
+    for filepath in image_paths:
+        if prefix in filepath:
+            histograms.append(make2DHist(filepath))
+
+    for i in range(len(image_paths)):
+        for j in range(i,len(histograms)):
+            # compare histogram to the current image histograms
+            if compareTwoHist(image_paths[i], histograms[j], emd) > THRESHOLD:
+                print image_paths[i] + 'class ' + str(j)
+                exit(0)
+
+            # else make new class
+         # f.write(compareTwoHist(image_paths[i],image_paths[j],cv2.compareHist, cv2.HISTCMP_CORREL )+ "\n")
