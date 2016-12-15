9c55f7cfed06a3f1ab9495a7e44136c8d068494e
Stephen Krewson <stephen.krewson@gmail.com>
2016-12-15 03:27:31 -0500

    matlab code posted

--- synset-classifier/reduce.m
+++ synset-classifier/reduce.m
@@ -13,10 +13,10 @@ figure, scatter3(mappedX(:,1), mappedX(:,2), mappedX(:,3)); title('Result of PCA
 
 [mappedX, mapping] = compute_mapping(matrix, 'Laplacian', no_dims, 7);	
 figure, scatter3(mappedX(:,1), mappedX(:,2), mappedX(:,3)); title('Result of Laplacian Eigenmaps'); drawnow
-size(mappedX)
-size(mapping)
 
-% Now cluster this!
+% Constrain to first three principal components, get the pointwise
+% distances and then do agglomerative single link clustering (cf. with
+% average, which actuall does better)
 X = mappedX(:,1:3);
 eucD = pdist2(X,X);
 clustTreeEuc = linkage(eucD,'average');
@@ -28,17 +28,27 @@ h_gca.TickDir = 'out';
 h_gca.TickLength = [.002 0];
 h_gca.XTickLabel = [];
 
+% Get the images; remember to offset by 2 to account for dir symlinks
+% we also want to make them all the same size
+location = '../img/thumbnails/';
+imagelist = dir(location);
 
 % https://www.mathworks.com/help/stats/examples/cluster-analysis.html
+% 1) create montages (cutoff depends on average vs. single-link)
 hidx = cluster(clustTreeEuc,'criterion','distance','cutoff',0.19);
-for i = 1:13
+for i = 1:no_dims
     clust = find(hidx==i);
-    clust
-    scatter3(X(clust,1),X(clust,2),X(clust,3));
+    filenames = {imagelist(clust+2).name};
+    absopaths = strcat(location,filenames);
+    figure, imdisp(absopaths); title(num2str(i));
+end
+
+% 2) create 3-D scatterplot of the laplacian eigenmap clusters; use two
+% loops since otherwise the figure handles would get crazy
+for i = 1:no_dims
+    clust = find(hidx==i);
+    scatter3(X(clust,1),X(clust,2),X(clust,3));title('Clusters');
     hold on
 end
 hold off
 grid on
-
-%[mappedX, mapping] = compute_mapping(matrix, 'DiffusionMaps', no_dims);	
-%figure, scatter(mappedX(:,1), mappedX(:,2)); title('This is the testing one'); drawnow
